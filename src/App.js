b536c937a4318f420cb299acd3c0137e6ed96131
sadiki-dev <sadiki.dev@gmail.com>
2021-08-09 16:15:03 +0200

    create Footer Component and add it to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Banner from './Components/Banner/Banner';
+import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="app">
       <Header />
       <Home />
+      <Footer />
       
 
       {/* Home Page */}
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p>© 2021 Airbnb clone! No Rights Reserved - This is a demo!</p>
+            <p>Privacy . Terms . Sitemap . Company Details</p>
+        </div>
+    )
+}
+
+export default Footer
