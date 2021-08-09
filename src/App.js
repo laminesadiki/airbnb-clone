06cd3c489089139d508c3823334d907311146913
sadiki-dev <sadiki.dev@gmail.com>
2021-08-09 19:46:27 +0200

    install react-router-dom package , add Route inside App

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import Banner from './Components/Banner/Banner';
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import SearchPage from './Components/SearchPapge/SearchPage';
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path="/search">
+              <SearchPage />
+          </Route>
+          <Route path="/" >
+              <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </BrowserRouter>
+
+      
+      
       
 
       {/* Home Page */}
